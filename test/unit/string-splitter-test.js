1a203f76c1e15720b19421de01d02bba759ce9f1
kobi.shitrit <kobi.shitrit@naturalint.com>
2020-08-11 19:02:45 +0300

    initial setup plus logic for stringSplitter, including tests

---
+++ test/unit/string-splitter-test.js
@@ -0,0 +1,133 @@
+const chai = require('chai');
+const { expect } = chai;
+
+const stringSplitter  = require('../../lib/business-logic/helpers/string-splitter');
+
+const expectResultToBeArray = (result) => {
+    expect(Array.isArray(result)).to.be.true;
+};
+
+const expectArraysToBeEqual = (expectedArray, resultArray) => {
+    expectResultToBeArray(resultArray);
+
+    expect(resultArray.length).to.eq(expectedArray.length);
+
+    for(let i = 0; i < resultArray.length; i++) {
+        expect(resultArray[i]).to.eq(expectedArray[i]);
+    }
+};
+
+describe('string-splitter', () => {
+
+    describe ('splitIntoWords', () => {
+
+        const { splitIntoWords } = stringSplitter;
+
+        it('returns an empty array when receiving an empty string', () => {
+            const result = splitIntoWords('');
+
+            expectArraysToBeEqual([], result);
+        });
+
+        it('returns an empty array when receiving an empty null or undefined values', () => {
+            let result;
+
+            result = splitIntoWords(null);
+            expectArraysToBeEqual([], result);
+
+            result = splitIntoWords();
+            expectArraysToBeEqual([], result);
+        });
+
+        it('returns an empty array when input string contains only special characters', () => {
+            const inputString = '!@#$%^&()';
+
+            const result = splitIntoWords('');
+
+            expectArraysToBeEqual([], result);
+        });
+
+        it('return an array with a single word when the string contains a single word.', () => {
+            const inputString = 'word';
+            const expectedResult = ['word'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('ignores leading and trailing whitespaces when input string contains a single word.', () => {
+            const inputString = '   \tword  \t ';
+            const expectedResult = ['word'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('return an array with a single word when the string contains a single word and special characters.', () => {
+            const inputString = '{[(word]?!)]},.~!@#$%^&*-=+|\\';
+            const expectedResult = ['word'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('return a correct array with each word in the input string when the string contains multiple words separated by whitespaces', () => {
+            const inputString = 'This is more than just one word';
+            const expectedResult = ['This', 'is', 'more', 'than', 'just', 'one', 'word'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('return a correct array with each word in the input string when the string contains multiple words and special characters', () => {
+            const inputString = '(This) is! more? [than] just, one !@#$%%^&*()-=+ word.';
+            const expectedResult = ['This', 'is', 'more', 'than', 'just', 'one', 'word'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('regards recurring words in the input strings as separate words', () => {
+            const inputString = 'Hello! hello! hello! lo lo o... Can anybody hear me? ear me? me? me?';
+            const expectedResult = ['Hello', 'hello', 'hello', 'lo', 'lo', 'o', 'Can', 'anybody', 'hear', 'me', 'ear', 'me', 'me', 'me'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('regards all special characters as separators', () => {
+            const inputString = 'My un-dead friend is b*witched';
+            const expectedResult = ['My', 'un', 'dead', 'friend', 'is', 'b', 'witched'];
+
+            const result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+        it('regards underscore as a character in a word, and not as a separator', () => {
+            let inputString;
+            let expectedResult;
+            let result;
+
+            inputString = '_';
+            expectedResult = ['_'];
+            result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+
+            inputString = '   _   ';
+            expectedResult = ['_'];
+            result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+
+            inputString = '-_!';
+            expectedResult = ['_'];
+            result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+
+            inputString = 'this is one_word!';
+            expectedResult = ['this', 'is', 'one_word'];
+            result = splitIntoWords(inputString);
+            expectArraysToBeEqual(expectedResult, result);
+        });
+
+    });
+});
