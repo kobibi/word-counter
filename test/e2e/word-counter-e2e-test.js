49a3458a50e63e3cacec02e9a3e3924d04de4681
kobi.shitrit <kobi.shitrit@naturalint.com>
2020-08-12 21:04:33 +0300

    added business logic for input of type string. No tests yest

---
+++ test/e2e/word-counter-e2e-test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+
+describe ('Word Counter service', () => {
+    describe ('word wounter end point', () => {
+        it('returns a 422 status code with structured response when input is incorrect', () => {});
+
+        it('returs a 200 status code with structured response when input is correct', async () => {
+        });
+    });
+});
